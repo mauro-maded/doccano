76f4a8a2484e25b439c9108b47ae065ae010cc7f
mauro-maded <mauro.maded@gmail.com>
2025-03-09 20:07:04 +0100

    chore: update Jest configuration for TypeScript support and improve coverage collection

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,16 +1,24 @@
 module.exports = {
+  preset: 'ts-jest',
+  moduleFileExtensions: ['js', 'ts', 'vue', 'json'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js',
     '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub'
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.vue$': 'vue-jest',
     '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub'
   },
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/components/**/*.vue', '<rootDir>/pages/**/*.vue']
-}
+  collectCoverageFrom: [
+      '<rootDir>/components/**/*.vue',
+      '<rootDir>/pages/**/*.vue',
+      '<rootDir>/pages/**/*.ts',
+      '<rootDir>/components/**/*.ts'
+    ],
+  testEnvironment: 'jsdom'
+};
